0473765f879256620f9c827307cbefd5a9126f86
Ryan Burnette <ryan.burnette@gmail.com>
2014-12-30 14:18:48 -0500

    Refactor and fix string concat typo

--- luckygrids.php
+++ luckygrids.php
@@ -7,20 +7,24 @@
  * Author: Ryan Burnette
  * Author URI: http://ryanburnette.com
  * Requires at least: 2.5
- * Tested up to: 4.1
+ * Tested up to: 4.0.1
  */
 
 function luckygrids_embed_code($id) {
-  return '<script src="//luckygrids.io/grids/'+$id+'.js" async="true"></script><div id="luckygrid'+$id+'"></div>';
+  return '<script src="//luckygrids.io/grids/'.$id.'.js" async="true"></script><div id="luckygrid'.$id.'"></div>';
+}
+
+function luckygrids_shortcode_atts($atts) {
+  $supported = array(
+    'id' => '0'
+  );
+  return shortcode_atts($supported, $atts);
 }
 
 function luckygrids_shortcode($atts) {
-  $defaults = array();
-  $a = shortcode_atts($defaults, $atts);
-  $id = $a['id'];
-  return luckygrids_embed_code($id);
+  $atts = luckygrids_shortcode_atts($atts);
+  return luckygrids_embed_code($atts['id']);
 }
 
 add_shortcode('luckygrid', 'luckygrids_shortcode');
 add_shortcode('luckygrids', 'luckygrids_shortcode');
-
